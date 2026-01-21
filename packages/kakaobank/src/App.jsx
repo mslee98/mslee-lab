95abbf515bd78a1d12a58491a6acc2bc63619dc7
이민성 <ms.lee@okestro.com>
2026-01-21 14:30:17 +0900

    test(kakaobank-splash) 테스트 프레이머 모션 스플래시 화면

--- packages/kakaobank/src/App.jsx
+++ packages/kakaobank/src/App.jsx
@@ -1,18 +1,35 @@
+import { useIOSBackSwipe } from './useIOSBackSwipe';
+import CustomCursor from './CustomCursor';
+import MobileViewport from './components/common/MobileViewport';
+import { MobileDevWrapper } from './components/common/MobileDevWrapper';
+import SplashScreen from './components/splash/SplashScreen';
+import { useState } from 'react';
+
 export default function KakaoBankApp() {
+  useIOSBackSwipe();
+
+  const [ready, setReady] = useState(false);
+
   return (
-    <>
-      <div className="
-        w-screen
-        h-screen
-        bg-red-500
-        flex
-        items-center
-        justify-center
-        text-[48px]
-        text-white
-      ">
-        Kakao Bank App
-      </div>
-    </>
+    <MobileDevWrapper>
+      <MobileViewport>
+        <CustomCursor />
+
+        {!ready && (
+          <SplashScreen onFinish={() => setReady(true)} />
+        )}
+
+        {ready && <MainScreen />}
+        <MainScreen />
+      </MobileViewport>
+    </MobileDevWrapper>
+  );
+}
+
+function MainScreen() {
+  return (
+    <div className="w-full h-full bg-red-500 flex items-center justify-center">
+      <span className="text-white text-4xl">Kakao Bank App</span>
+    </div>
   );
 }
